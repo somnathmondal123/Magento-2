701a4017e6e495e0f3889d14eb3086d2979ea7fa
Stanilsav Tsygankov <s.tsygankov@balticsolutions.lv>
2017-08-11 02:49:14 +0300

    refactoring

--- Controller/Checkout/Cancel.php
+++ Controller/Checkout/Cancel.php
@@ -19,30 +19,29 @@ class Cancel extends \Icepay\IcpCore\Controller\AbstractCheckout
     {
         try {
 
+            //TODO: check if payment was canceled or failed
+
+
             // if there is an order - cancel it
             $orderId = $this->getCheckoutSession()->getLastOrderId();
             /** @var \Magento\Sales\Model\Order $order */
             $order = $orderId ? $this->_orderFactory->create()->load($orderId) : false;
 
-            if($this->cancelOrder($order, 'ICEPAY Checkout and Order have been canceled.'))
-            {
-                $this->messageManager->addSuccessMessage(
-                    __('ICEPAY Checkout and Order have been canceled.')
-                );
-            }
-            else
-            {
-                $this->messageManager->addSuccessMessage(
-                    __('ICEPAY Checkout has been canceled.')
-                );
+            $message = 'Checkout has been canceled.';
+            if ($order && $this->cancelOrder($order, $message)) {
+                $message = 'Checkout and Order have been canceled.';
             }
 
+            $this->messageManager->addSuccessMessage(__($message));
+            $this->logger->debug(sprintf($message.' Order ID: %s', $orderId ? '' : $orderId));
 
             $this->getCheckoutSession()->restoreQuote();
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
             $this->messageManager->addExceptionMessage($e, $e->getMessage());
+            $this->logger->debug("Cancel.php: " . $e->getMessage());
         } catch (\Exception $e) {
             $this->messageManager->addExceptionMessage($e, __('Unable to cancel ICEPAY Checkout'));
+            $this->logger->debug("Cancel.php: " . $e->getMessage());
         }
 
         /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
@@ -50,5 +49,5 @@ class Cancel extends \Icepay\IcpCore\Controller\AbstractCheckout
         return $resultRedirect->setPath('checkout/cart');
     }
 
-    
+
 }
